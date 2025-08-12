21338e15fa16c8800e1f6c8244f0c1fae37f79a9
fray osorio <frayosorio@gmail.com>
2025-08-12 16:57:28 -0500

    Se agrega proyecto FRONT END en angular

--- bd/DDL - Festivos.sql
+++ bd/DDL - Festivos.sql
@@ -29,3 +29,23 @@ CREATE TABLE Festivo(
 	IdTipo INT NOT NULL,
 	CONSTRAINT fkFestivo_Tipo FOREIGN KEY (IdTipo) REFERENCES Tipo(Id)
 	);
+
+--Crear secuencias
+CREATE SEQUENCE secuencia_pais
+    INCREMENT 1
+    START 1
+    MINVALUE 1
+    OWNED BY pais.id;
+
+CREATE SEQUENCE secuencia_tipo
+    INCREMENT 1
+    START 1
+    MINVALUE 1
+    OWNED BY tipo.id;
+
+
+CREATE SEQUENCE secuencia_festivo
+    INCREMENT 1
+    START 1
+    MINVALUE 1
+    OWNED BY festivo.id;
